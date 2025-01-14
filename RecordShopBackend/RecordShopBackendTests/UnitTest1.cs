a3a2534f6db4dadfca97ff67d63700cb199c6c9a
termgiles <Thomas.giles2222@gmail.com>
2025-01-14 11:37:24 +0000

    test: add testing project

---
+++ RecordShopBackend/RecordShopBackendTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RecordShopBackendTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
