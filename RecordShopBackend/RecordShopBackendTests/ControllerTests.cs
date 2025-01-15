7cfda1ef9dcbda6b0d298183df55ab658bafc765
termgiles <Thomas.giles2222@gmail.com>
2025-01-15 11:03:31 +0000

    test: controller GetAlbumById tests

--- RecordShopBackend/RecordShopBackendTests/ControllerTests.cs
+++ RecordShopBackend/RecordShopBackendTests/ControllerTests.cs
@@ -13,6 +13,12 @@ namespace RecordShopBackendTests
         private Mock<IRecordShopService> _mockService;
         private RecordShopController _controller;
 
+        List<Album> testAlbums = new List<Album>
+            {
+                new Album { Id = 1, Artist = "L'Ed Sheeran", Genre = "La Pop", Information = "un album du chanteur Ed Sheeran nommé d'après un symbiole mathématique", Name = "L'addition", Released = 2011},
+                new Album{ Id = 2, Artist = "L'Ed Sheeran", Genre = "La Pop", Information = "un album du chanteur Ed Sheeran nommé d'après un symbiole mathématique", Name = "La multiplication", Released = 2014},
+            };
+
         [SetUp]
         public void Setup()
         {
@@ -50,11 +56,6 @@ namespace RecordShopBackendTests
         public void GetAllAlbums_ReturnsOkWithAlbums()
         {
             // Arrange
-            var testAlbums = new List<Album>
-            {
-                new Album { Id = 1, Artist = "L'Ed Sheeran", Genre = "La Pop", Information = "un album du chanteur Ed Sheeran nommé d'après un symbiole mathématique", Name = "L'addition", Released = 2011},
-                new Album{ Id = 2, Artist = "L'Ed Sheeran", Genre = "La Pop", Information = "un album du chanteur Ed Sheeran nommé d'après un symbiole mathématique", Name = "La multiplication", Released = 2014},
-            };
             _mockService.Setup(r => r.ReturnAllAlbums()).Returns(testAlbums);
 
             // Act
@@ -64,6 +65,52 @@ namespace RecordShopBackendTests
             result.Should().BeOfType<OkObjectResult>()
                 .Which.StatusCode.Should().Be((int)HttpStatusCode.OK);
             result.As<OkObjectResult>().Value.Should().Be(testAlbums);
+        }
+
+        [Test]
+        public void GetAlbumById_ReturnsOkWithFoundAlbums()
+        {
+            // Arrange
+            var expected = new AlbumReturn { Found = true, ReturnedObject = testAlbums[0] };
+            _mockService.Setup(r => r.ReturnAlbumById(1)).Returns(expected);
+
+            // Act
+            var result = _controller.GetAlbumById(1);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>()
+                .Which.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            result.As<OkObjectResult>().Value.Should().BeEquivalentTo(expected.ReturnedObject);
+        }
+
+        [Test]
+        public void GetAlbumById_QueriesServiceLayerOnce()
+        {
+            // Arrange
+            var expected = new AlbumReturn { Found = true, ReturnedObject = testAlbums[0] };
+            _mockService.Setup(r => r.ReturnAlbumById(1)).Returns(expected);
+
+            // Act
+            var result = _controller.GetAlbumById(1);
+
+            // Assert
+            _mockService.Verify(r => r.ReturnAlbumById(1), Times.Once());
+
+        }
+
+        [Test]
+        public void GetAlbumById_Returns404IfNoAlbum()
+        {
+            // Arrange
+            var expected = new AlbumReturn { Found = false, ReturnedObject = null };
+            _mockService.Setup(r => r.ReturnAlbumById(70)).Returns(expected);
+
+            // Act
+            var result = _controller.GetAlbumById(70);
+
+            // Assert
+            result.Should().BeOfType<NotFoundResult>()
+                .Which.StatusCode.Should().Be((int)HttpStatusCode.NotFound);;
         }
     }
 }
