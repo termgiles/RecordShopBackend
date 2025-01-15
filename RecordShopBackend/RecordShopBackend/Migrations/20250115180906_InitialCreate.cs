a71ea83e8af5c2e7bd2ea1bcabccb3a5b8c85411
termgiles <Thomas.giles2222@gmail.com>
2025-01-15 18:11:28 +0000

    faet: db migration

---
+++ RecordShopBackend/RecordShopBackend/Migrations/20250115180906_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RecordShopBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Artist = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Released = table.Column<int>(type: "int", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Information = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Albums",
+                columns: new[] { "Id", "Artist", "Genre", "Information", "Name", "Released" },
+                values: new object[,]
+                {
+                    { 1, "L'Ed Sheeran", "La Pop", "un album du chanteur Ed Sheeran nommé d'après un symbiole mathématique", "L'addition", 2011 },
+                    { 2, "L'Ed Sheeran", "La Pop", "un album du chanteur Ed Sheeran nommé d'après un symbiole mathématique", "La multiplication", 2014 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albums");
+        }
+    }
+}
